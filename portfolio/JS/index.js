76a94fb8b7912f4399b30424522403149f0aea33
Cameron <festivecam@gmail.com>
2025-01-04 15:18:26 -0500

    Inital Push for portfolio

---
+++ portfolio/JS/index.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open'); /* when a link is opened in the menu, remove the nav-open from event listener? */
+    })
+})
